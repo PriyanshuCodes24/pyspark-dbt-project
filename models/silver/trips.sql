8229a1cea5f4e18d1a73d97eb56365f45dc11683
Priyanshu Kumar Upadhyay <priyanshuupadhyay694@gmail.com>
2026-01-16 16:31:30 +0000

    Development Completed Gold Layer

---
+++ models/silver/trips.sql
@@ -0,0 +1,27 @@
+{{ config(
+    materialized='incremental',
+    unique_key='trip_id'
+) }}
+
+{% set cols = [
+    'trip_id',
+    'driver_id',
+    'customer_id',
+    'vehicle_id',
+    'trip_start_time',
+    'trip_end_time',
+    'end_location',
+    'distance_km',
+    'fare_amount',
+    'last_updated_timestamp'] %}
+
+select
+{% for col in cols %}
+    {{ col }}{% if not loop.last %},{% endif %}
+{% endfor %}
+from {{ source('bronze', 'trips') }}
+
+{% if is_incremental() %}
+where last_updated_timestamp >
+    (select coalesce(max(last_updated_timestamp), '1900-01-01' )from {{ this }})
+{% endif %}
